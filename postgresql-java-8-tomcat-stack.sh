321831c2222f83332c4f0750bed983e431ba1877
Jonathan Leijendekker <lejendary@users.noreply.github.com>
2018-12-07 13:45:08 +0800

    Update postgresql-java-8-tomcat-stack.sh

--- postgresql-java-8-tomcat-stack.sh
+++ postgresql-java-8-tomcat-stack.sh
@@ -79,7 +79,7 @@ Environment=JAVA_HOME=/usr/lib/jvm/java-8-oracle/
 Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
 Environment=CATALINA_HOME=/opt/tomcat
 Environment=CATALINA_BASE=/opt/tomcat
-Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+Environment="CATALINA_OPTS=-Xms512M -Xmx8192M -server -XX:+UseParallelGC"
 Environment="JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom"
 
 ExecStart=/opt/tomcat/bin/startup.sh
